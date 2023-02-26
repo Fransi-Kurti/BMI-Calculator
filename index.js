08b4d1c8fc6cf33a45b9984cf70d7649d8097439
fransi-kurti <fransi.kurti@gmail.com>
2023-02-26 17:12:37 +0100

    finished javascript part

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const btnEl = document.getElementById("btn");
 
+const bmiResult = document.getElementById("bmi-result");
+
+const weightConditionEl = document.getElementById("weight-condition");
+
 function calculateBMI(){
-    const heightInputValue = document.getElementById("height-input").value;
+    const heightInputValue = document.getElementById("height-input").value / 100;
     const weightInputValue = document.getElementById("weight-input").value;
-    console.log(heightInputValue, weightInputValue);
+
+    const bmiValue = weightInputValue / (heightInputValue * heightInputValue);
+    
+    bmiResult.value = bmiValue;
+
+    if(bmiValue < 18.5){
+        weightConditionEl.innerText = "Under weight"
+    }else if(bmiValue >= 18.5 && bmiValue <= 24.9){
+        weightConditionEl.innerText = "Normal weight"
+    }else if(bmiValue >= 25 && bmiValue <= 29.9){
+        weightConditionEl.innerText = "Over weight"
+    }else if(bmiValue >= 30){
+        weightConditionEl.innerText = "Obesity";
+    }
+    
 }
 
 
